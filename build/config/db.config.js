76e93203819e284c2ff4febae1e427c600984442
Paul <pool7795@gmail.com>
2024-05-19 13:04:50 -0500

    [ADD]fixed

--- build/config/db.config.js
+++ build/config/db.config.js
@@ -1,16 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const pg_1 = require("pg");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const pool = new pg_1.Pool({
-    user: process.env["DB_USER"] || "paul",
-    host: process.env["DB_HOST"] || "localhost",
-    database: process.env["DB_NAME"] || "csv_data",
-    password: process.env["DB_PASSWORD"] || "gatito",
-    port: parseInt(process.env["DB_PORT"] || "5432", 10),
+    user: process.env["DB_USER"],
+    host: process.env["DB_HOST"],
+    database: process.env["DB_NAME"],
+    password: process.env["DB_PASSWORD"],
+    port: Number(process.env["DB_PORT"]),
 });
 exports.default = pool;
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,14 +1,12 @@
+import "dotenv/config";
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const pool = new Pool({
-  user: process.env["DB_USER"] || "paul",
-  host: process.env["DB_HOST"] || "localhost",
-  database: process.env["DB_NAME"] || "csv_data",
-  password: process.env["DB_PASSWORD"] || "gatito",
-  port: parseInt(process.env["DB_PORT"] || "5432", 10),
+  user: process.env["DB_USER"],
+  host: process.env["DB_HOST"],
+  database: process.env["DB_NAME"],
+  password: process.env["DB_PASSWORD"],
+  port: Number(process.env["DB_PORT"]),
 });
 
 export default pool;
